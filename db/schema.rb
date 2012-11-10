904ed077fcd77628958c2d83d96a658e2538abcf
Steve Derico <steve@stevederico.com>
2012-11-10 14:02:17 -0800

    Make basic User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121105171428) do
+ActiveRecord::Schema.define(:version => 20121110212048) do
 
   create_table "foods", :force => true do |t|
     t.string   "bar"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(:version => 20121105171428) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,9 @@ describe "User pages" do
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
+
+  describe "When password is not present" do
+  	before { @user.password = @user.password_confirmation = " "}
+  	it { should_not be_valid }
+  end
 end
